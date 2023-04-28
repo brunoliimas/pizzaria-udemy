67f99d88095b064c5f67698707e0790f764463ca
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-04-28 16:16:37 -0300

    Configurando o envio da imagem

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -5,19 +5,28 @@ class CreateProductController {
     async handle(req: Request, res: Response) {
         const { name, price, description, category_id } = req.body;
 
-        let banner = '';
-
         const createProductController = new CreateProductService();
 
-        const product = await createProductController.execute({
-            name,
-            price,
-            description,
-            banner,
-            category_id,
-        });
+        if (!req.file) {
+            throw new Error("error upload file")
+        } else {
+            const { originalname, filename} = req.file
 
-        return res.json(product);
+            console.log(filename);
+            
+
+            const product = await createProductController.execute({
+                name,
+                price,
+                description,
+                banner: '',
+                category_id,
+            });
+            return res.json(product);
+        }
+
+
+
     }
 }
 export { CreateProductController }
