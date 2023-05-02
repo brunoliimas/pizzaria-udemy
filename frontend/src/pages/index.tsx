e8e4f49f5e08f0b8d12175f426c1cf223f2bdd13
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-02 17:21:29 -0300

    Criando contexto de autenticação

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useContext } from "react"
+
 import Head from "next/head"
 import Link from "next/link"
 
@@ -5,7 +7,22 @@ import { Input } from "@/components/ui/Input"
 import { Button } from "@/components/ui/Button"
 import { Logo } from "@/components/ui/Logo"
 
+import { AuthContext } from "@/contexts/AuthContext"
+
+
 export default function Home() {
+
+    const { signIn } = useContext(AuthContext)
+
+    async function handleLogin(event: FormEvent) {
+        event.preventDefault();
+
+        let data = {
+            email: "teste@gmail.com",
+            password: "123456"
+        }
+        await signIn(data)
+    }
     return (
         <>
             <Head>
@@ -15,7 +32,7 @@ export default function Home() {
                 <Logo />
                 <h1 className="mb-4 text-2xl font-bold text-white">Entrar</h1>
                 <div className="w-full max-w-xs">
-                    <form className="flex flex-col items-center justify-center">
+                    <form onSubmit={handleLogin} className="flex flex-col items-center justify-center">
                         <Input type="text" placeholder="Digite seu e-mail" />
                         <Input type="password" placeholder="Digite sua senha" />
                         <Button
