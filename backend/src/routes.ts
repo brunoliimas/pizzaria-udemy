8c76da81aefff14510c263445f0f0029b8b9481f
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-02 00:15:18 -0300

    Criando um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,8 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 import uploadConfig from './config/multer';
@@ -35,6 +37,8 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
+// Rotas Order
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
 
 
 
