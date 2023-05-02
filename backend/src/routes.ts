9e16ac494cd82f694b4be586a1ad752fd15590a4
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-02 02:13:42 -0300

    Detalhes do Pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,6 +17,7 @@ import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrdersController } from './controllers/order/ListOrdersController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
 
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
@@ -51,6 +52,7 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 router.get('/orders', isAuthenticated, new ListOrdersController().handle);
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
 
 
 
