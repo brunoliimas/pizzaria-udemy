a707a4125558fb52195a05665fdb68e7fdca5499
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-11 14:58:35 -0300

    Cadastrando categorias

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -1,14 +1,27 @@
 import { useState, FormEvent } from 'react'
+import Head from "next/head"
 
 import Header from "@/components/Header"
-import Head from "next/head"
+import { setupAPIClient } from '@/services/api';
+import { toast } from 'react-toastify';
+
 
 export default function Category() {
     const [name, setName] = useState('');
 
     async function handleRegister(event: FormEvent) {
         event.preventDefault();
-        alert("categoria " + name)
+        if (name === '') {
+            return;
+        }
+
+        const apiClient = setupAPIClient();
+        await apiClient.post('/category', {
+            name: name
+        });
+        toast.success("Categoria cadastrada com sucesso");
+
+        setName('');
     }
 
 
