217a60cacc3a1e884fc361f3b0b464b5c4307500
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-11 16:22:48 -0300

    Criando o componente preview da imagem do produto

--- frontend/src/pages/products/index.tsx
+++ frontend/src/pages/products/index.tsx
@@ -1,9 +1,32 @@
+import {useState, ChangeEvent} from 'react'
 import Head from "next/head"
 
 import { canSSRAuth } from '@/utils/canSSRAuth';
 import Header from "@/components/Header";
 
+import { FiUpload } from 'react-icons/fi'
+
 export default function Product() {
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState<File | null>(null);
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+        if(!e.target.files){
+            return;
+        }
+
+        const image = e.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+        }
+    }
+
     return (
         <>
             <Head>
@@ -14,6 +37,21 @@ export default function Product() {
                 <h1 className="text-white text-4xl font-bold mb-10 text-center">Cadastrar novo produto</h1>
 
                 <form className="flex flex-col w-full md:w-1/2 max-w-[600px]">
+
+                    <label className="w-full h-[220px] flex items-center justify-center mb-4 rounded-sm bg-dark-900 border border-gray-100 cursor-pointer overflow-hidden">
+                        <span className="z-50 absolute opacity-50 transition-all duration-500 hover:scale-150 hover:opacity-100">
+                            <FiUpload size={30} color="#fff" />
+                        </span>
+                        <input className="hidden" type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+
+                        {avatarUrl && (
+                            <img
+                            className='w-full'
+                            src={avatarUrl}
+                            alt="Foto do produto"/>
+                        )}
+                    </label>
+
 
                     <select className="w-full mb-4 rounded-sm bg-dark-900 text-white p-4 border border-gray-100 placeholder:text-white placeholder:opacity-50" >
                         <option>Bebidas</option>
