c93fd53bae3135cef7564be3ef45f807d3d3948e
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-12 16:31:08 -0300

    Cadastrando o produto

--- frontend/src/pages/products/index.tsx
+++ frontend/src/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import Head from 'next/head';
 
 import { canSSRAuth } from '@/utils/canSSRAuth';
@@ -6,6 +6,7 @@ import Header from '@/components/Header';
 
 import { FiUpload } from 'react-icons/fi';
 import { setupAPIClient } from '@/services/api';
+import { toast } from 'react-toastify';
 
 interface ItemProps {
     id: string;
@@ -17,6 +18,9 @@ interface CategoryProps {
 }
 
 export default function Product({ categoryList }: CategoryProps) {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
     const [avatarUrl, setAvatarUrl] = useState('');
     const [imageAvatar, setImageAvatar] = useState<File | null>(null);
     const [categories, setCategories] = useState<ItemProps[]>(categoryList || []);
@@ -40,6 +44,41 @@ export default function Product({ categoryList }: CategoryProps) {
         const selectedIndex = Number(e.target.value);
         setCategorySelected(selectedIndex);
     }
+
+    async function handleRegister(event: FormEvent) {
+        event.preventDefault();
+
+        try {
+            const data = new FormData();
+            if (name === '' || price === '' || description === '' || imageAvatar === null) {
+                toast.warning("Preencha todos os campos");
+                return;
+            }
+            data.append('name', name);
+            data.append('price', price);
+            data.append('description', description);
+            data.append('category_id', categories[categorySelected].id);
+            data.append('file', imageAvatar);
+
+            const apiClient = setupAPIClient();
+
+            await apiClient.post('/product', data);
+
+            toast.success('Produto cadstrado com sucesso!')
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Ops, erro ao cadastrar!!!");
+
+        }
+        setName('');
+        setPrice('');
+        setDescription('');
+        setImageAvatar(null);
+        setAvatarUrl('');
+
+
+    }
     return (
         <>
             <Head>
@@ -49,7 +88,7 @@ export default function Product({ categoryList }: CategoryProps) {
             <main className="container mx-auto px-4 flex flex-col items-center justify-center mt-16">
                 <h1 className="text-white text-4xl font-bold mb-10 text-center">Cadastrar novo produto</h1>
 
-                <form className="flex flex-col w-full md:w-1/2 max-w-[600px]">
+                <form className="flex flex-col w-full md:w-1/2 max-w-[600px]" onSubmit={handleRegister}>
 
                     <label className="w-full h-[220px] flex items-center justify-center mb-4 rounded-sm bg-dark-900 border border-gray-100 cursor-pointer overflow-hidden">
                         <span className="z-50 absolute opacity-50 transition-all duration-500 hover:scale-150 hover:opacity-100">
@@ -59,7 +98,7 @@ export default function Product({ categoryList }: CategoryProps) {
 
                         {avatarUrl && (
                             <img
-                                className='w-full'
+                                className='w-full object-contain'
                                 src={avatarUrl}
                                 alt="Foto do produto" />
                         )}
@@ -80,15 +119,21 @@ export default function Product({ categoryList }: CategoryProps) {
                     </select>
 
                     <input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         type="text"
                         placeholder="Digite o nome do produto"
                         className="w-full mb-4 h-10 rounded-sm bg-dark-900 text-white p-4 border border-gray-100 placeholder:text-white placeholder:opacity-50" />
                     <input
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
                         type="text"
                         placeholder="Digite o preço do produto"
                         className="w-full mb-4 h-10 rounded-sm bg-dark-900 text-white p-4 border border-gray-100 placeholder:text-white placeholder:opacity-50" />
 
                     <textarea
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
                         className="w-full mb-4 rounded-sm bg-dark-900 text-white p-4 border border-gray-100 placeholder:text-white placeholder:opacity-50 resize-none"
                         placeholder="Descreva seu produto..."></textarea>
 
@@ -105,9 +150,9 @@ export default function Product({ categoryList }: CategoryProps) {
     )
 }
 export const getServerSideProps = canSSRAuth(async (ctx: any) => {
-    const apliClient = setupAPIClient(ctx);
+    const apiClient = setupAPIClient(ctx);
 
-    const response: any = await apliClient.get('/category');
+    const response: any = await apiClient.get('/category');
 
     return {
         props: {
