b7ff42c9c1dc8d64edb5eef6cd7c898e579e58ab
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2023-05-13 03:06:15 -0300

    Buscando Pedidos

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
 import Head from "next/head"
 
 import { Logo } from "@/components/ui/Logo"
 import { canSSRAuth } from "@/utils/canSSRAuth"
 import Header from "@/components/Header"
 import { FiRefreshCcw } from 'react-icons/fi'
+import { setupAPIClient } from "@/services/api"
 
-export default function Dashboard() {
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+
+interface HomeProps {
+    orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps) {
+
+    const [orderList, setOrderList] = useState(orders || []);
+
+    function handleOpenModalView(id: string){
+        alert("Clicou Bee no " + id)
+    }
+
     return (
         <>
             <Head>
@@ -20,33 +41,29 @@ export default function Dashboard() {
                     </button>
                 </div>
                 <article className="flex flex-col my-4">
-                    <section className="flex bg-dark-900 mb-4 py-2 px-4 items-center rounded-sm before:block before:absolute before:w-2 relative before:top-0 before:left-0 before:bg-green-900 before:h-full before:text-green-900 before:rounded-l-sm">
-                        <button className="text-white">
-                            {/* <div className="bg-green-900 w-1 h-full"></div> */}
-                            <span>Mesa 25</span>
-                        </button>
-                    </section>
-                    <section className="flex bg-dark-900 mb-4 py-2 px-4 items-center rounded-sm before:block before:absolute before:w-2 relative before:top-0 before:left-0 before:bg-green-900 before:h-full before:text-green-900 before:rounded-l-sm">
-                        <button className="text-white">
-                            {/* <div className="bg-green-900 w-1 h-full"></div> */}
-                            <span>Mesa 12</span>
-                        </button>
-                    </section>
-                    <section className="flex bg-dark-900 mb-4 py-2 px-4 items-center rounded-sm before:block before:absolute before:w-2 relative before:top-0 before:left-0 before:bg-green-900 before:h-full before:text-green-900 before:rounded-l-sm">
-                        <button className="text-white">
-                            {/* <div className="bg-green-900 w-1 h-full"></div> */}
-                            <span>Mesa 97</span>
-                        </button>
-                    </section>
-                    
+                    {orderList.map(item => (
+                        <section key={item.id} className="flex bg-dark-900 mb-4 py-2 px-4 items-center rounded-sm before:block before:absolute before:w-2 relative before:top-0 before:left-0 before:bg-green-900 before:h-full before:text-green-900 before:rounded-l-sm">
+                            <button onClick={() => handleOpenModalView(item.id)} className="text-white">
+                                <span>Mesa {item.table}</span>
+                            </button>
+                        </section>
+                    ))}
                 </article>
             </main>
         </>
     )
 }
 
-export const getServerSideProps = canSSRAuth(async (ctx) => {
+export const getServerSideProps = canSSRAuth(async (ctx: any) => {
+    const apiClient = setupAPIClient(ctx);
+
+    const response = await apiClient.get('/orders');
+
+    // console.log(response.data);
+
     return {
-        props: {}
+        props: {
+            orders: response.data
+        }
     }
 })
